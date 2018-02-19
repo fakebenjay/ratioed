7422d06d57f60059579b940e06e15462824d1200
fakebenjay <fakebenjay@gmail.com>
2018-02-19 02:36:17 -0500

    basic templates and models built

--- badtweets/views.py
+++ badtweets/views.py
@@ -6,20 +6,23 @@ from IPython import embed
 from .models import Tweet, User
 
 # Create your views here.
-tweets = []
 
 def index(request):
-    return HttpResponse("Hello, world. Here are some shitty tweets!")
+    return render(request, 'index.html')
 
 def detail(request, username, date=dt.date.today()):
+    tweets = []
+    user = ""
+
     response = "You are looking at %s's bad tweets"
     counter = 1
 
-    for tweet in twitterscraper.query_tweets("from%3A" + username, limit=50, poolsize=1, begindate=dt.date(2006,3,21), enddate=date):
+    for tweet in twitterscraper.query_tweets("from%3A" + username, limit=100, poolsize=1, begindate=dt.date(2006,3,21), enddate=date):
+        embed()
         if (int(tweet.replies) > 2*int(tweet.retweets)) and (int(tweet.replies) >= 1.5*int(tweet.likes)) and (int(tweet.replies) >= 30):
-            embed()
             t = Tweet(tweet_id=tweet.id, body=tweet.text, link="http://twitter.com/" + tweet.user + "/status/" + tweet.id, datetime=tweet.timestamp, replies=tweet.replies, rts=tweet.retweets, likes=tweet.likes)
             tweets.append(t)
-            counter += 1
+        counter += 1
 
-    return HttpResponse(tweets[1])
+
+    return HttpResponse(tweets[0].body)
