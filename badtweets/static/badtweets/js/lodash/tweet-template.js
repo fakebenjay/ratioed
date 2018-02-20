d841af07459bccad59e91d65f7a6b5740832b7db
fakebenjay <fakebenjay@gmail.com>
2018-02-20 16:26:06 -0500

    HOT FRESH TWEETS COMING UP

--- badtweets/static/badtweets/js/lodash/tweet-template.js
+++ badtweets/static/badtweets/js/lodash/tweet-template.js
@@ -2,22 +2,19 @@ function postTweets(name) {
   var tweetTemplate = $('#tweet-template').html()
   var templateFn = _.template(tweetTemplate)
 
-  var tweetDiv = $('#tweet-div')
-
   var badTweets = []
 
   tweetCall(name, function(raw_json) {
     json = JSON.parse(raw_json)
 
-    debugger
-
     for (i=0; i<json.length; i++) {
-      tweetDiv += templateFn({
+      console.log(i)
+      document.getElementById('tweet-div').innerHTML += templateFn({
         'handle': json[i].fields.handle,
         'name': json[i].fields.name,
         'body': json[i].fields.body,
         'replies': json[i].fields.replies,
-        'retweets': json[i].fields.retweets,
+        'retweets': json[i].fields.rts,
         'likes': json[i].fields.likes,
         'datetime': json[i].fields.datetime
       })
