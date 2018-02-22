b683ef29d4a1b83ee89c6ee69a148a299340fc33
fakebenjay <fakebenjay@gmail.com>
2018-02-22 17:10:00 -0500

    started manual janky error handling in fetch

--- badtweets/static/badtweets/js/wrapper.js
+++ badtweets/static/badtweets/js/wrapper.js
@@ -1,11 +1,18 @@
 function tweetCall(paramsObj, callback) {
   if (!!paramsObj.date) {
-    fetch(`http://localhost:8000/badtweets/?date=${paramsObj.date}&username=${paramsObj.name}`)
-      .then(res => res.json())
-      .then(json => callback(json))
+    var url = `http://localhost:8000/badtweets/?date=${paramsObj.date}&username=${paramsObj.name}`
   } else {
-    fetch(`http://localhost:8000/badtweets/?username=${paramsObj.name}`)
-      .then(res => res.json())
-      .then(json => callback(json))
+    var url = `http://localhost:8000/badtweets/?username=${paramsObj.name}`
   }
+
+  fetch(url)
+    .then(function(res) {
+      if (res.status !== 200) {
+        document.querySelector('.w-30').innerHTML += `<br></br><em style='color:red;'>Please enter a valid user</em>`
+        return "error"
+      }
+      return res
+    })
+    .then(res => res.json())
+    .then(json => callback(json))
 }
