f27816d4279e7d1e277f331fd22e9a6127baea5a
fakebenjay <fakebenjay@gmail.com>
2018-02-24 23:09:52 -0500

    ssl redirect

--- ratioed/settings.py
+++ ratioed/settings.py
@@ -143,3 +143,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'badtweets/static'),
 )
+
+##Automati HTTPS
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
