e143286daa7aa43ba0baaf07f5fba19ee391dcdf
fakebenjay <fakebenjay@gmail.com>
2018-09-29 20:01:55 -0400

    static url fix 2

--- ratioed/settings.py
+++ ratioed/settings.py
@@ -147,7 +147,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/badtweets'
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
