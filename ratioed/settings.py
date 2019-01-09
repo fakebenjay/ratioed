985a4f2107fa6e957251d8c3c19993baac33d125
fakebenjay <fakebenjay@gmail.com>
2019-01-09 03:23:48 -0500

    help

--- ratioed/settings.py
+++ ratioed/settings.py
@@ -184,7 +184,7 @@ CORS_ALLOW_METHODS = (
 
 
 # import os
-#
+
 # # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #
